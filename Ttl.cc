1fe886d16acbc53186d49ca2b081bb3a9499f89f
Thura Naing <thuran@bu.edu>
2026-04-26 18:05:48 -0400

    better random for ttl values

--- Ttl.cc
+++ Ttl.cc
@@ -1,23 +1,12 @@
-#include "Ttl.h"
 #include <vector>
 #include <random>
 
 int ttl_generator() {
-    // ttl choices
-    srand(422);
+    static std::mt19937 gen(422); 
+    
     static std::vector<int> choices = {7, 30, 365};
-    // weights
-    static std::vector<double> weights = {0.3, 0.5, 0.2};
+    
+    static std::discrete_distribution<size_t> dist({0.3, 0.5, 0.2});
 
-    double r = (double)rand() / RAND_MAX;
-
-    double cumulative = 0.0;
-    for (size_t i = 0; i < weights.size(); ++i) {
-        cumulative += weights[i];
-        if (r < cumulative) {
-            return choices[i];
-        }
-    }
-
-    return choices.back();
+    return choices[dist(gen)];
 }

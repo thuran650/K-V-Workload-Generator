30367747d6a7cf650623142d07fce74447fb4388
Thura Naing <thuran@bu.edu>
2026-05-04 16:09:23 -0400

    workload script

--- Ttl.cc
+++ Ttl.cc
@@ -4,7 +4,7 @@
 int ttl_generator(long total_ttl_deletes) {
     static std::vector<int> choices = {88, 137, 275};
     
-    static std::discrete_distribution<size_t> dist({0.1, 0.3, 0.6});
+    static std::discrete_distribution<size_t> dist({1.0, 0, 0});
 
     static long generated = 0;
     long current = generated++;
---
+++ generate_experiment.sh
@@ -0,0 +1,65 @@
+BASE_DIR=$(pwd)
+mkdir experiments
+cd experiments
+mkdir lethe
+mkdir rocksdb
+
+cd lethe
+mkdir 10
+mkdir 8
+mkdir 6
+mkdir 4
+mkdir 2
+mkdir 0
+
+cd ..
+
+cd rocksdb
+mkdir 10
+mkdir 8
+mkdir 6
+mkdir 4
+mkdir 2
+mkdir 0
+
+cd "$BASE_DIR"
+make
+
+# LETHE
+./load_gen -I 450000 -T 50000 -E 1024 
+cp workload.txt experiments/lethe/10
+
+./load_gen -I 460000 -T 40000 -E 1024 
+cp workload.txt experiments/lethe/8
+
+./load_gen -I 470000 -T 30000 -E 1024 
+cp workload.txt experiments/lethe/6
+
+./load_gen -I 480000 -T 20000 -E 1024 
+cp workload.txt experiments/lethe/4
+
+./load_gen -I 490000 -T 10000 -E 1024 
+cp workload.txt experiments/lethe/2
+
+./load_gen -I 500000 -E 1024 
+cp workload.txt experiments/lethe/0
+
+
+# ROCKS
+./load_gen -I 450000 -D 50000 -E 1024 
+cp workload.txt experiments/rocksdb/10
+
+./load_gen -I 460000 -D 40000 -E 1024 
+cp workload.txt experiments/rocksdb/8
+
+./load_gen -I 470000 -D 30000 -E 1024 
+cp workload.txt experiments/rocksdb/6
+
+./load_gen -I 480000 -D 20000 -E 1024 
+cp workload.txt experiments/rocksdb/4
+
+./load_gen -I 490000 -D 10000 -E 1024 
+cp workload.txt experiments/rocksdb/2
+
+./load_gen -I 500000 -E 1024 
+cp workload.txt experiments/rocksdb/0
